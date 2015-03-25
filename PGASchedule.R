6400435c2aac0916485c9a72ab899a763a9398d6
kcanez <kevin.canez@gmail.com>
2015-03-24 21:06:53 -0700

    Delete PGASchedule.R

--- PGASchedule.R
+++
@@ -1,6 +0,0 @@
-library('jsonlite')
-JSON_Schedule <- fromJSON(txt = "http://www.pgatour.com/data/R/schedule.json")
-x <- JSON_Schedule$tours$trns
-DF <- as.data.frame(x[[1]])
-DF[16] <- DF[16]$trnName$official
-DF[c(5,9, 16)]
